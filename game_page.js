4293f0c50cbf012c91c4e0ce776a4097e75c0a1f
Daksh1111 <83402774+Daksh1111@users.noreply.github.com>
2021-11-10 20:17:46 +1000

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -30,4 +30,67 @@ player2_name = localStorage.getItem("player2_name");
  }
 
 
+ question_turn = "player1";
+ answer_turn = "player2";
+
+function check() {
+
+      get_Answer = document.getElementById("input_check_box").value;
+      if (get_Answer == actual_answer){
+
+            if(answer_turn == "player1"){
+                  player1_score = player1_score + 1;
+                  document.getElementById("player1_score").innerHTML= player1_score;
+            }
+            else {
+                  player2_score = player2_score + 1;
+                  document.getElementById("player2_score").innerHTML= player2_score;
+            }
+      }
+
+      if(question_turn == "player1"){
+            
+            question_turn = "player2";
+            document.getElementById("player_question").innerHTML="Question Turn - " + player2_name;
+
+      }
+      else {
+            
+            question_turn = "player1";
+            document.getElementById("player_question").innerHTML="Question Turn - " + player1_name;
+
+      }
+
+      if(answer_turn == "player1"){
+
+            answer_turn = "player2";
+            document.getElementById("player_answer").innerHTML="Answer Turn - " + player2_name;
+
+      }
+      else {
+            
+            answer_turn = "player1";
+            document.getElementById("player_answer").innerHTML="Answer Turn - " + player1_name;
+      }
+
+      document.getElementById("output").innerHTML="";
+
+      if(player1_score == 5){
+            document.getElementById("number4").innerHTML= player1_name + " Wins";
+            document.getElementById("number3").innerHTML= player2_name + " Loses";
+            document.getElementById("Check").innerHTML= "Refresh To Play Again";
+
+      }
+      
+
+      if(player2_score == 5){
+            document.getElementById("number3").innerHTML= player2_name + " Wins";
+            document.getElementById("number4").innerHTML= player1_name + " Loses";
+            document.getElementById("Check").innerHTML= "Refresh To Play Again";
+      }
+
+
+}
+ 
+
       
